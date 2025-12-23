369d1358452e0dd88770cfdacca9f49c30ca9adf
deiz0n <eduardomacedosilva@outlook.com>
2025-12-23 11:53:43 -0300

    feat: criação do GetAllClassesService

---
+++ apps/school-system/src/class/application/services/get-all-classes.service.ts
@@ -0,0 +1,15 @@
+import { Inject } from '@nestjs/common';
+import { Class } from '../../domain/class.model';
+import { GetAllClassesUseCase } from '../ports/in/get-all-classes.use-case';
+import { ClassRepositoryPort } from '../ports/out/class.repository.port';
+
+class GetAllClassesService implements GetAllClassesUseCase {
+  constructor(
+    @Inject(ClassRepositoryPort)
+    private readonly repository: ClassRepositoryPort,
+  ) {}
+
+  async execute(): Promise<Class[]> {
+    return this.repository.getAllClasses();
+  }
+}
