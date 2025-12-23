093f96624b32b323348666e4a96f6b9282a2044a
deiz0n <eduardomacedosilva@outlook.com>
2025-12-23 11:20:37 -0300

    feat: criação do resource class

--- apps/school-system/src/app.module.ts
+++ apps/school-system/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
+import { ClassModule } from './class/class.module';
 
 @Module({
-  imports: [StudentModule],
+  imports: [StudentModule, ClassModule],
   controllers: [AppController],
   providers: [AppService],
 })
