efbd5d9a86bb8aa84d00b54d2bc92975139af672
deiz0n <eduardomacedosilva@outlook.com>
2025-12-23 19:32:01 -0300

    chore: configuração do student module

--- apps/school-system/src/student/student.module.ts
+++ apps/school-system/src/student/student.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { StudentService } from './student.service';
-import { StudentController } from './student.controller';
+import { StudentController } from './infrastructure/adapters/in/web/student.controller';
+import { GetStudentsByClassService } from './application/services/get-students-by-class.service';
+import { StudentRepositoryPort } from './application/ports/out/student.repository.port';
+import { TypeOrmStudentAdapter } from './infrastructure/adapters/out/persistence/typeorm-student.adapter';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentEntity } from './infrastructure/adapters/out/persistence/student.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StudentEntity])],
   controllers: [StudentController],
-  providers: [StudentService],
+  providers: [
+    { provide: GetStudentsByClassService, useClass: GetStudentsByClassService },
+    { provide: StudentRepositoryPort, useClass: TypeOrmStudentAdapter },
+  ],
 })
 export class StudentModule {}
