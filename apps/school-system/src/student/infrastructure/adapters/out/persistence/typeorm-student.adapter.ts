8178ad2932b34684391346a13b86d2bb9ba748c3
deiz0n <eduardomacedosilva@outlook.com>
2025-12-23 19:23:49 -0300

    feat: criação do typeorm adapter

---
+++ apps/school-system/src/student/infrastructure/adapters/out/persistence/typeorm-student.adapter.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { StudentRepositoryPort } from '../../../../application/ports/out/student.repository.port';
+import { Repository } from 'typeorm';
+import { StudentEntity } from './student.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StudentModel } from '../../../../domain/student.model';
+
+@Injectable()
+export class TypeOrmClassAdapter implements StudentRepositoryPort {
+  constructor(
+    @InjectRepository(StudentEntity)
+    private readonly repository: Repository<StudentEntity>,
+  ) {}
+
+  getStudentsByClass(class_id: string): Promise<StudentModel[]> {
+    return this.repository.find({
+      where: {
+        classes: { id: class_id },
+      },
+      relations: ['student'],
+    });
+  }
+}
