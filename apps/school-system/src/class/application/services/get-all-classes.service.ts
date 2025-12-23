e7f8cd9bbf8ee820151dfeff580eab48ab7abc2a
deiz0n <eduardomacedosilva@outlook.com>
2025-12-23 15:41:35 -0300

    feat: export do GetAllClassesService para uso no module

--- apps/school-system/src/class/application/services/get-all-classes.service.ts
+++ apps/school-system/src/class/application/services/get-all-classes.service.ts
@@ -3,7 +3,7 @@ import { Class } from '../../domain/class.model';
 import { GetAllClassesUseCase } from '../ports/in/get-all-classes.use-case';
 import { ClassRepositoryPort } from '../ports/out/class.repository.port';
 
-class GetAllClassesService implements GetAllClassesUseCase {
+export class GetAllClassesService implements GetAllClassesUseCase {
   constructor(
     @Inject(ClassRepositoryPort)
     private readonly repository: ClassRepositoryPort,
