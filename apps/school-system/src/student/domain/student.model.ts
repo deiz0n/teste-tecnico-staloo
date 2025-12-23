bd1bc43ae4c279e3f67817a770e8a3e1ef2a7a1f
deiz0n <eduardomacedosilva@outlook.com>
2025-12-23 18:20:48 -0300

    fix: ajustes no nomes dos atributos address e dateBirth

--- apps/school-system/src/student/domain/student.model.ts
+++ apps/school-system/src/student/domain/student.model.ts
@@ -3,7 +3,7 @@ export class StudentModel {
     public readonly id: string,
     public name: string,
     public cpf: string,
-    public endereco: string,
-    public dataNascimento: Date,
+    public address: string,
+    public dateBirth: Date,
   ) {}
 }
