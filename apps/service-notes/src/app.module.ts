5b1a4999ed7e408654d5aecc421b027681b71686
deiz0n <eduardomacedosilva@outlook.com>
2025-12-24 13:52:42 -0300

    chore: configurações para conexão com banco de dados

---
+++ apps/service-notes/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('SERVICE_NOTES_DB_HOST'),
+        port: configService.get<number>('SERVICE_NOTES_DB_PORT'),
+        username: configService.get<string>('SERVICE_NOTES_DB_USER'),
+        password: configService.get<string>('SERVICE_NOTES_DB_PASSWORD'),
+        database: configService.get<string>('SERVICE_NOTES_DB'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
