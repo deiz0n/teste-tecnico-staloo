efdb7e22d3087794c068d2b15f8e4e4aec6c0308
deiz0n <eduardomacedosilva@outlook.com>
2025-12-26 11:06:22 -0300

    feat: adição dos atributos date e subject, e o renomeação do atributo student_id

--- apps/service-notes/src/exam/domain/exam.model.ts
+++ apps/service-notes/src/exam/domain/exam.model.ts
@@ -1,7 +1,11 @@
+import { SubjectModel } from '../../subject/domain/subject.model';
+
 export class ExamModel {
   constructor(
     public readonly id: string,
     public score: number,
-    public studentId: string,
+    public date: Date,
+    public student_id: string,
+    public subject: SubjectModel,
   ) {}
 }
