de3a63bbdc0a91b6259c24caac59a3d70da03a58
deiz0n <eduardomacedosilva@outlook.com>
2025-12-26 11:30:17 -0300

    chore: configuração do module e remoção dos componentes não utilizados

--- apps/service-notes/src/exam/exam.module.ts
+++ apps/service-notes/src/exam/exam.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ExamService } from './exam.service';
-import { ExamController } from './exam.controller';
+import { GetAllByStudentUseCase } from './application/ports/in/get-all-by-student.use-case';
+import { GetAllByStudentService } from './application/services/get-all-by-student.service';
+import { ExamRepositoryPort } from './application/ports/out/exam.repository.port';
+import { TypeOrmExamAdapter } from './infrastructure/adapters/out/persistence/typeorm-exam.adapter';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExamEntity } from './infrastructure/adapters/out/persistence/exam.entity';
+import { ExamController } from './infrastructure/adapters/in/web/exam.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ExamEntity])],
   controllers: [ExamController],
-  providers: [ExamService],
+  providers: [
+    { provide: GetAllByStudentUseCase, useClass: GetAllByStudentService },
+    { provide: ExamRepositoryPort, useClass: TypeOrmExamAdapter },
+  ],
 })
 export class ExamModule {}
