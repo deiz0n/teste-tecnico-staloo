03f2e53d73d300f195d8436f52f3c4f6bef8d1b3
deiz0n <eduardomacedosilva@outlook.com>
2025-12-27 17:25:27 -0300

    feat: criação do endpoint para geração do boletim

--- apps/school-system/src/student/infrastructure/adapters/in/web/student.controller.ts
+++ apps/school-system/src/student/infrastructure/adapters/in/web/student.controller.ts
@@ -3,15 +3,18 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  Param,
   ParseUUIDPipe,
   Query,
 } from '@nestjs/common';
 import { GetStudentsByClassUseCase } from '../../../../application/ports/in/get-students-by-class.use-case';
+import { GenerateReportCardUseCase } from '../../../../application/ports/in/generate-report-card.use-case';
 
 @Controller('students')
 export class StudentController {
   constructor(
     private readonly getStudentsByClassUseCase: GetStudentsByClassUseCase,
+    private readonly generateReportCardUseCase: GenerateReportCardUseCase,
   ) {}
 
   @Get()
@@ -21,6 +24,17 @@ export class StudentController {
     return {
       timestamp: new Date().toISOString(),
       data: data,
+    };
+  }
+
+  @Get(':studentId/report')
+  async generateReportCard(
+    @Param('studentId', ParseUUIDPipe) studentId: string,
+  ) {
+    const data = await this.generateReportCardUseCase.execute(studentId);
+    return {
+      timestamp: new Date().toISOString(),
+      date: data,
     };
   }
 }
