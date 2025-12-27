11a6259f4356cd8fdac1effc89c3fdd45bbb450f
deiz0n <eduardomacedosilva@outlook.com>
2025-12-27 19:15:57 -0300

    fix: ajuste no caminho do seed do script e o seed foi movido para a raiz do microserviço

--- apps/service-notes/seed.ts
+++ apps/service-notes/seed.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { AppModule } from './app.module';
-import { ExamEntity } from './exam/infrastructure/adapters/out/persistence/exam.entity';
-import { SubjectEntity } from './subject/infrastructure/adapters/out/persistence/subject.entity';
+import { AppModule } from './src/app.module';
+import { ExamEntity } from './src/exam/infrastructure/adapters/out/persistence/exam.entity';
+import { SubjectEntity } from './src/subject/infrastructure/adapters/out/persistence/subject.entity';
+import { randomUUID } from 'crypto';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
@@ -32,7 +33,14 @@ async function bootstrap() {
     workload: 90,
   });
 
+  const subjectEntity2 = subjectRepo.create({
+    id: randomUUID(),
+    name: 'Biologia',
+    workload: 120,
+  });
+
   await subjectRepo.save(subjectEntity);
+  await subjectRepo.save(subjectEntity2);
 
   const examEntity = examRepo.create({
     id: '84dd54fb-57c4-41fb-8c39-540ddcc65c80',
@@ -42,7 +50,25 @@ async function bootstrap() {
     subject: subjectEntity,
   });
 
+  const examEntity3 = examRepo.create({
+    id: randomUUID(),
+    score: 10,
+    date: new Date(),
+    student_id: '7b16e374-58ba-4995-a0ba-96980760c427',
+    subject: subjectEntity,
+  });
+
+  const examEntity2 = examRepo.create({
+    id: randomUUID(),
+    score: 10,
+    date: new Date(),
+    student_id: '7b16e374-58ba-4995-a0ba-96980760c427',
+    subject: subjectEntity2,
+  });
+
   await examRepo.save(examEntity);
+  await examRepo.save(examEntity2);
+  await examRepo.save(examEntity3);
 
   console.info('✅ Seed Service Notes successfully completed');
 
