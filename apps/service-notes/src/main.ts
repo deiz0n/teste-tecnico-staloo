07bbd5ff148871b81e7008b31d57e55c2cd3fac6
deiz0n <eduardomacedosilva@outlook.com>
2025-12-30 17:42:26 -0300

    fix: ajuste na definição da porta da aplicação e adição de log para inicialização

--- apps/service-notes/src/main.ts
+++ apps/service-notes/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
   const config = app.get(ConfigService);
-  const port = config.get<number>('SERVICES_NOTES_PORT') || 3001;
-  await app.listen(port);
+  const port = config.get<number>('SERVICE_NOTES_PORT');
+
+  await app.listen(3000);
+
+  logger.log(
+    `Microservice Service Notes is running in http://localhost:${port}`,
+  );
 }
 bootstrap();
