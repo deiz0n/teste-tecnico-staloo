f3513bc8899c9832f670cf0e4bf7037ef548ec74
deiz0n <eduardomacedosilva@outlook.com>
2025-12-30 18:13:04 -0300

    chore: configuração do module e remoção de componentes não utilizados

--- apps/service-notes/src/academic-record/academic-record.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { AcademicRecordService } from './academic-record.service';
-
-@Controller('academic-record')
-export class AcademicRecordController {
-  constructor(private readonly academicRecordService: AcademicRecordService) {}
-}
--- apps/service-notes/src/academic-record/academic-record.module.ts
+++ apps/service-notes/src/academic-record/academic-record.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AcademicRecordService } from './academic-record.service';
-import { AcademicRecordController } from './academic-record.controller';
+import { AcademicRecordController } from './infrastructure/adapters/in/academic-record.controller';
+import { GetStudentReportCardUseCase } from './application/ports/in/get-student-report-card.use-case';
+import { GetStudentReportCardService } from './application/services/get-student-report.-card.service';
+import { TypeOrmAcademicRecordAdapter } from './infrastructure/adapters/out/persistence/typeorm.academic-record.adapter';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AcademicRecordEntity } from './infrastructure/adapters/out/persistence/academic-record.entity';
+import { AcademicReportRepositoryPort } from './application/ports/out/academic-report.repository.port';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AcademicRecordEntity])],
   controllers: [AcademicRecordController],
-  providers: [AcademicRecordService],
+  providers: [
+    {
+      provide: GetStudentReportCardUseCase,
+      useClass: GetStudentReportCardService,
+    },
+    {
+      provide: AcademicReportRepositoryPort,
+      useClass: TypeOrmAcademicRecordAdapter,
+    },
+  ],
 })
 export class AcademicRecordModule {}
