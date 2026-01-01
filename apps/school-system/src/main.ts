af50c716a6ea3e0431dc1fe4d3425b85010d3999
deiz0n <eduardomacedosilva@outlook.com>
2026-01-01 16:03:22 -0300

    chore: ajuste na definição da porta da api e adição de log para inicialização

--- apps/school-system/src/main.ts
+++ apps/school-system/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
   const config = app.get(ConfigService);
   const port = config.get<number>('SCHOOL_SYSTEM_PORT ') || 3000;
-  await app.listen(port);
+
+  await app.listen(3000);
+
+  logger.log(
+    `Microservice School System is running in http://localhost:${port}`,
+  );
 }
 bootstrap();
